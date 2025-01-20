e2a72ff782ceabef34542323228b5931c936272b
Aditya Maru <adityamaru@gmail.com>
2025-01-20 10:36:37 -0500

    *: revert external cache to be backed by blob store
    Previously, we moved the external cache to sticky disks
    from blob store without fully appreciating the nuances of its
    implementation. This broke some use cases of the external cache.
    Since we want to be a drop in replacement, and since our colocated
    cache already offers ~400MB/s download speeds, it is better to keep
    the upstream behaviour intact and revert just the external cache to
    use our blob store.
    
    There is a path in the future where we can load the entire `external/`
    bazel dir on a stickydisk, but this would break the use case where a
    user specifies a custom `manifest` in the `external-cache` config, to
    define what files are used to compute the hash for the cache key. There
    are solutions to this too, but they all over-complicate the code for
    not very much in the way of performance gains.

--- dist/stickydisk/index.js
+++ dist/stickydisk/index.js
@@ -25899,7 +25899,51 @@ function createStickyDiskClient() {
   return createClient(StickyDiskService, transport);
 }
 
-module.exports = { createStickyDiskClient }
+async function getFolderSize(rootItemPath, options = {}) {
+  const fileSizes = new Map()
+
+  await processItem(rootItemPath)
+
+  async function processItem(itemPath) {
+    if (options.ignore?.test(itemPath)) return
+
+    const stats = lstatSync(itemPath, { bigint: true })
+    if (typeof stats !== 'object') return
+
+    fileSizes.set(stats.ino, stats.size)
+
+    if (stats.isDirectory()) {
+      const directoryItems = fs.readdirSync(itemPath)
+      if (typeof directoryItems !== 'object') return
+      await Promise.all(
+        directoryItems.map(directoryItem =>
+          processItem(path.join(itemPath, directoryItem))
+        )
+      )
+    }
+  }
+
+  let folderSize = Array.from(fileSizes.values()).reduce((total, fileSize) => total + fileSize, 0n)
+
+  if (!options.bigint) {
+    if (folderSize > BigInt(Number.MAX_SAFE_INTEGER)) {
+      throw new RangeError('The folder size is too large to return as a Number. You can instruct this package to return a BigInt instead.')
+    }
+    folderSize = Number(folderSize)
+  }
+
+  return folderSize
+}
+
+function lstatSync(path, opts) {
+  try {
+    return fs.lstatSync(path, opts)
+  } catch (error) {
+    return
+  }
+}
+
+module.exports = { createStickyDiskClient, getFolderSize, lstatSync }
 
 /***/ }),
 
--- post.js
+++ post.js
@@ -1,11 +1,18 @@
 const core = require('@actions/core')
 const { unmountAndCommitStickyDisk } = require('./stickydisk')
-const process = require('node:process');
+const process = require('node:process')
+const path = require('node:path')
+const fs = require('node:fs')
+const glob = require('@actions/glob')
+const cache = require('@actions/cache')
+const config = require('./config')
+const { getFolderSize } = require('./util')
 
 async function run() {
   try {
     // Handle sticky disk unmounting and committing.
     await cleanupStickyDisks()
+    await saveExternalCaches(config.externalCache)
   } catch (error) {
     core.setFailed(error.message)
   }
@@ -26,6 +33,78 @@ async function cleanupStickyDisks() {
   await Promise.all(Object.entries(mounts).map(async ([path, mountInfo]) => {
     await unmountAndCommitStickyDisk(path, mountInfo, mountInfo.stickyDiskKey)
   }))
+}
+
+async function saveExternalCaches(cacheConfig) {
+  if (!cacheConfig.enabled) {
+    return
+  }
+
+  const globber = await glob.create(
+    `${config.paths.bazelExternal}/*`,
+    { implicitDescendants: false }
+  )
+  const externalPaths = await globber.glob()
+  const savedCaches = []
+
+  for (const externalPath of externalPaths) {
+    const size = await getFolderSize(externalPath)
+    const sizeMB = (size / 1024 / 1024).toFixed(2)
+    core.debug(`${externalPath} size is ${sizeMB}MB`)
+
+    if (sizeMB >= cacheConfig.minSize) {
+      const name = path.basename(externalPath)
+      await saveCache({
+        enabled: cacheConfig[name]?.enabled ?? cacheConfig.default.enabled,
+        files: cacheConfig[name]?.files || cacheConfig.default.files,
+        name: cacheConfig.default.name(name),
+        paths: cacheConfig.default.paths(name)
+      })
+      savedCaches.push(name)
+    }
+  }
+
+  if (savedCaches.length > 0) {
+    const path = cacheConfig.manifest.path
+    fs.writeFileSync(path, savedCaches.join('\n'))
+    await saveCache({
+      enabled: true,
+      files: cacheConfig.manifest.files,
+      name: cacheConfig.manifest.name,
+      paths: [path]
+    })
+  }
+}
+
+async function saveCache(cacheConfig) {
+  if (!cacheConfig.enabled) {
+    return
+  }
+
+  const cacheHit = core.getState(`${cacheConfig.name}-cache-hit`)
+  core.debug(`${cacheConfig.name}-cache-hit is ${cacheHit}`)
+  if (cacheHit === 'true') {
+    return
+  }
+
+  try {
+    core.startGroup(`Save cache for ${cacheConfig.name}`)
+    const paths = cacheConfig.paths
+    const hash = await glob.hashFiles(
+      cacheConfig.files.join('\n'),
+      undefined,
+      // We don't want to follow symlinks as it's extremely slow on macOS.
+      { followSymbolicLinks: false }
+    )
+    const key = `${config.baseCacheKey}-${cacheConfig.name}-${hash}`
+    core.debug(`Attempting to save ${paths} cache to ${key}`)
+    await cache.saveCache(paths, key)
+    core.info('Successfully saved cache')
+  } catch (error) {
+    core.warning(error.stack)
+  } finally {
+    core.endGroup()
+  }
 }
 
 run()
